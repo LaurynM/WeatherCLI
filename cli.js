dadae05002e3775c5171ab99b0a8f30fb30029ce
Lauryn McFarland <lauryn@Lauryns-MacBook-Pro.local>
2018-01-17 19:24:51 -0600

    inital files

---
+++ cli.js
@@ -0,0 +1,11 @@
+var user = require("./user.js");
+var admin = require("./admin.js");
+var request = require("request");
+const fs = require('fs');
+
+switch (process.argv[2]){
+  case "user":
+  break;
+  case "admin":
+  break;
+}
